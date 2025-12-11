c175d48acf1e2108ce3a200012212761ddc15586
COllison <toomanydays1612@gmail.com>
2025-12-11 05:04:52 +0000

    started Extreme Points

--- Lecture 16/Exr-Extreme Points/main.go
+++ Lecture 16/Exr-Extreme Points/main.go
@@ -5,18 +5,31 @@ Date Completed: 12/10/25
 Description: This program will list the coordinate points in a file and minimum and maximum X & Y points.
 ***************************************/
 /*
-
-
-
+object oriented. 
+GetPoints will -> parse and gen a Point obj from each line -> use slice to hold all ->v
+-> close file -> return slice
 */
-
-
-
-
 
 package main
 
 import "fmt"
+
+type Point struct {
+	Xcord int
+	Ycord int
+}
+
+func NewPoint(x int, y int) Point{
+	var p Point
+	p.Xcord = x
+	p.Ycord = y
+	return p
+}
+
+func GetPoints() {
+
+}
+
 
 func main() {
    
